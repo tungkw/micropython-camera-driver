7e5b2d8b673d34d72ad43fc3c6bbccf1be80cbdc
tungkw <914989053@qq.com>
2023-10-14 10:12:59 -0700

    build for new env

--- boards/ESP32_CAM/mpconfigboard.cmake
+++ boards/ESP32_CAM/mpconfigboard.cmake
@@ -1,3 +1,13 @@
+# Set location of base MicroPython directory.
+if(NOT MICROPY_DIR)
+    get_filename_component(MICROPY_DIR ${CMAKE_CURRENT_LIST_DIR}/../../../.. ABSOLUTE)
+endif()
+
+# Set location of the ESP32 port directory.
+if(NOT MICROPY_PORT_DIR)
+    get_filename_component(MICROPY_PORT_DIR ${MICROPY_DIR}/ports/esp32 ABSOLUTE)
+endif()
+
 set(SDKCONFIG_DEFAULTS
     boards/sdkconfig.base
     boards/ESP32_CAM/sdkconfig.esp32cam
